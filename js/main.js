0851954bc68a1884b21d6bdef69fb3d40fe3471f
jiePing <674336892@qq.com>
2017-04-05 09:57:36 +0800

    修改配置

--- js/main.js
+++ js/main.js
@@ -2,36 +2,42 @@ define([],function(){
     //配置
     require.config({
         baseUrl:'./',
-        paths:{
-            advertAdd:'js/advert/advert_add',
-            advertList:'js/advert/advert_list',
-            courseAdd1:'js/course/course_step1',
-            courseAdd2:'js/course/course_step2',
-            courseAdd3:'js/course/course_step3',
-            courseAdd:'js/course/course_add',
-            courseCategoryAdd:'js/course/course_category',
-            courseList:'js/course/course_list',
-            courseTopic:'js/course/course_topic',
-            login:'js/home/login',
-            repass:'js/home/repass',
-            settings:'js/home/settigns',
-            teacherAdd:'js/teacher/teacher_add',
-            teacherList:'js/teacher/teacher_list',
-            userProfile:'js/user/user_list',
-            userList:'js/user/user_profile',
 
-            //配置第三方js模块别名
-            template:'lib/artTemplate/template-debug',
-            bootstrap:'lib/bootstrap/js/bootstrap',
-            datepicker:'lib/bootstrap-datepicker/js/bootstrap-datepicker',
-            ckeditor:'lib/ckeditor/ckeditor',
-            ckeditorLand:'lib/ckeditor/lang/zh-cn',
-            echarts:'lib/echarts/echarts.min',
-            jquery:'lib/jquery.min',
-            jqueryCookie:'lib/jquery-cookie/jquery.cookie',
-            jqueryForm:'lib/jquery-region/jquery.region',
-            nprogress:'lib/nprogress/nprogress'
-        },
+        paths: {
+			
+			// 配置自己写的js模块别名
+			advertAdd: 'js/advert/advert_add',
+			advertList: 'js/advert/advert_list',
+			courseAdd1: 'js/course/course_add_step1',
+			courseAdd2: 'js/course/course_add_step2',
+			courseAdd3: 'js/course/course_add_step3',
+			courseAdd: 'js/course/course_add',
+			courseCategoryAdd: 'js/course/course_category_add',
+			courseCategory: 'js/course/course_category',
+			courseList: 'js/course/course_list',
+			courseTopic: 'js/course/course_topic',
+			login: 'js/home/login',
+			repass: 'js/home/repass',
+			settings: 'js/home/settings',
+			teacherAdd: 'js/teacher/teacher_add',
+			teacherList: 'js/teacher/teacher_list',
+			userProfile: 'js/user/user_profile',
+			userList: 'js/user/user_list',
+			
+			// 配置第三方js模块别名
+			template: 'lib/artTemplate/template-debug',
+			bootstrap: 'lib/bootstrap/js/bootstrap',
+			datepicker: 'lib/bootstrap-datepicker/js/bootstrap-datepicker',
+			ckeditor: 'lib/ckeditor/ckeditor',
+			ckeditorLand: 'lib/ckeditor/lang/zh-cn',
+			echarts: 'lib/echarts/echarts.min',
+			jquery: 'lib/jquery/jquery.min',
+			jqueryCookie: 'lib/jquery-cookie/jquery.cookie',
+			jqueryForm: 'lib/jquery-form/jquery.form',
+			jqueryRegion: 'lib/jquery-region/jquery.region',
+			nprogress: 'lib/nprogress/nprogress',
+		},
+
 
         shim:{
             // bootstrap是非define定义的模块，优依赖于jquery，所以要配置
